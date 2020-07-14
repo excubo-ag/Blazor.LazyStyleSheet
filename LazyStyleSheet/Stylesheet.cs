56bea647ca29198a02434644d889f80bdd077960
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-07-14 10:27:47 +0200

    fix condition

--- LazyStyleSheet/Stylesheet.cs
+++ LazyStyleSheet/Stylesheet.cs
@@ -14,7 +14,7 @@ namespace Excubo.Blazor.LazyStyleSheet
         {
             if (firstRender && Src != null)
             {
-                var condition = $"document.head.querySelector(`[src='{Src}']`) == null";
+                var condition = $"document.head.querySelector(`[href='{Src}']`) == null";
                 var action = $"let s = document.createElement('link'); s.setAttribute('rel', 'stylesheet'); s.setAttribute('href', '{Src}'); document.head.appendChild(s);";
                 await js.InvokeVoidAsync("eval", $"if ({condition}) {{ {action} }}");
                 render_required = false;
