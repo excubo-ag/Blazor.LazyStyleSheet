bb082a716fd4a08beb3ccc418509daf859b1a858
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-08-12 09:58:22 +0200

    fix test

--- Tests_LazyStyleSheet/LazyLoadTest.cs
+++ Tests_LazyStyleSheet/LazyLoadTest.cs
@@ -1,4 +1,4 @@
-﻿using Bunit.Mocking.JSInterop;
+﻿using Bunit.TestDoubles.JSInterop;
 using Excubo.Blazor.LazyStyleSheet;
 using NUnit.Framework;
 using System.Linq;
@@ -10,7 +10,7 @@ namespace Excubo.Blazor.Tests_LazyStyleSheet
         [Test]
         public void Test()
         {
-            var js = Services.AddMockJsRuntime();
+            var js = Services.AddMockJSRuntime();
             Bunit.IRenderedComponent<Stylesheet>? cut;
             Assert.DoesNotThrow(() => cut = RenderComponent<Stylesheet>((name: "Src", value: "hello.css")));
             Assert.IsTrue(js.VerifyInvoke("eval").Arguments.Single().ToString().Contains("hello.css"));
