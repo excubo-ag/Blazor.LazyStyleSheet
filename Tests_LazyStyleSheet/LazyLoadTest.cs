d224cbffe6c2d76084aab95ddafe5848e158e087
Stefan Lörwald <stefan.loerwald@gmail.com>
2020-12-30 14:44:00 +0100

    update bunit

--- Tests_LazyStyleSheet/LazyLoadTest.cs
+++ Tests_LazyStyleSheet/LazyLoadTest.cs
@@ -1,4 +1,5 @@
-﻿using Bunit.TestDoubles.JSInterop;
+﻿using Bunit;
+using Bunit.JSInterop;
 using Excubo.Blazor.LazyStyleSheet;
 using NUnit.Framework;
 using System.Linq;
@@ -10,10 +11,14 @@ namespace Excubo.Blazor.Tests_LazyStyleSheet
         [Test]
         public void Test()
         {
-            var js = Services.AddMockJSRuntime();
-            Bunit.IRenderedComponent<Stylesheet>? cut;
+            IRenderedComponent<Stylesheet>? cut = null;
+            JSInterop.SetupVoid("eval", (i) => true);
             Assert.DoesNotThrow(() => cut = RenderComponent<Stylesheet>((name: "Src", value: "hello.css")));
-            Assert.IsTrue(js.VerifyInvoke("eval").Arguments.Single().ToString().Contains("hello.css"));
+            CollectionAssert.IsNotEmpty(JSInterop.Invocations);
+            var verification = JSInterop.VerifyInvoke("eval");
+            Assert.IsNotNull(verification);
+            CollectionAssert.IsNotEmpty(verification.Arguments);
+            Assert.IsTrue(verification.Arguments.First().ToString()!.Contains("hello.css"));
         }
     }
 }
